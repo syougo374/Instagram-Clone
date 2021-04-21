1a158d154f9a12945a2dab5771757579c5fed8f8
syougo374 <supernaturalsyougo@gmail.com>
2021-04-21 01:03:48 +0000

    400,500エラー表示追加、タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,14 @@ Bundler.require(*Rails.groups)
 module InstagramClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.load_defaults 5.1
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
